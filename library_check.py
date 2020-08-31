c640f24c71147dbd5c5720f8e802169d17ffdd1e
Your Name <you@example.com>
2020-08-31 17:26:09 +0900

    For ICU students

---
+++ library_check.py
@@ -0,0 +1,23 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import feedparser
+import os
+
+flag = 0
+url = 'https://opac.icu.ac.jp/opac/newbook_rss/?lang=0&dptidpl=1&jfcd=&sort=&cls=&clskey='
+
+rss = feedparser.parse(url)
+
+latest = rss.entries[0].title
+
+with open('check.txt', mode='r')as f:
+    tmp = f.read()
+    if tmp != latest:
+        flag = 1
+        with open('check.txt', mode='w')as g:
+            g.write(latest)
+
+if flag:
+    os.system("osascript -e 'display notification \"New Books Arrive!\"'")
+else:
+    pass
